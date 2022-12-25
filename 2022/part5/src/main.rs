a71b2e073872911a6993f09365042f54bdb61a15
Yukinori <38091245+YukiTamashi@users.noreply.github.com>
2022-12-24 21:59:29 -0300

    day5 part 1

--- 2022/part5/src/main.rs
+++ 2022/part5/src/main.rs
@@ -1,10 +1,31 @@
 use std::fs;
 
+#[derive(Debug)]
+struct Move{
+    amount: usize,
+    from: usize,
+    to: usize,
+}
+
+impl From<&str> for Move{
+    fn from(s: &str) -> Self {
+        let mut moves = vec!();
+        for word in s.split_whitespace(){
+            if let Ok(n) = word.parse::<usize>(){
+                moves.push(n);
+            }
+        }
+        if moves.len() != 3{
+            panic!()
+        }
+        Move { amount: moves[0], from: moves[1] -1, to: moves[2] -1}
+    }
+}
+
 fn main() {
     let moves = fs::read_to_string("day5.in").unwrap();
     let stacks = fs::read_to_string("stacks.in").unwrap();
     let mut stacks = build_stack(&stacks);
-    println!("{:?}", stacks);
     solve_moves(&mut stacks, &moves);
     println!("{:?}", stacks);
 }
@@ -13,7 +34,7 @@ fn main() {
 fn build_stack(s: &str) -> Vec<Vec<char>>{
     let mut result: Vec<Vec<char>> = vec!();
     let a = s.lines().map(|x| x.chars().collect::<Vec<char>>()).collect::<Vec<Vec<char>>>();
-    println!("{:#?}", a);
+    // println!("{:#?}", a);
     for (i, c) in a[8].iter().enumerate(){
         if *c != ' '{
             let mut new = vec!();
@@ -29,24 +50,21 @@ fn build_stack(s: &str) -> Vec<Vec<char>>{
     result
 }
 
-fn solve_moves(stacks: &mut Vec<Vec<char>>, moves: &str){
+fn solve_moves(stacks: &mut [Vec<char>], moves: &str){
+    let mut moves_vec = vec!();
     for line in moves.lines(){
-        let mut moves = vec!();
-        for word in line.split_whitespace(){
-            if let Ok(n) = word.parse::<usize>(){
-                moves.push(n);
-            }
-        }
-        println!("{:?}", moves);
-        for _ in 0..moves[0]{
-            transfer(stacks, moves[1], moves[2]);
-        }
+        moves_vec.push(Move::from(line));
     }
+    transfer(stacks, moves_vec)
 }
 
-fn transfer(stacks: &mut Vec<Vec<char>>, from: usize, to: usize) {
-    let c = stacks[from-1].pop();
-    if let Some(c) = c{
-        stacks[to-1].push(c);
+fn transfer(stacks: &mut [Vec<char>], moves: Vec<Move>) {
+    for m in moves{
+        for _ in 0..m.amount{
+            let c = stacks[m.from].pop();
+            if let Some(c) = c{
+                stacks[m.to].push(c);
+            }
+        }
     }
 }
